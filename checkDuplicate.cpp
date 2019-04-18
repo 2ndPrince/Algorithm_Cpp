60c59ddb5922d1b02f17fe192f6c8c2962db27a3
YOUNGSEOK LEE <astonisher88@gmail.com>
2019-04-18 09:54:30 -0500

    Update checkDuplicate.cpp

--- checkDuplicate.cpp
+++ checkDuplicate.cpp
@@ -1,10 +1,12 @@
+// The hash set is one of the implementations of a set which is a data structure to store no repeated values. 
+// https://leetcode.com/explore/learn/card/hash-table/183/combination-with-other-algorithms/1130/
+
 /*
 Given an array of integers, find if the array contains any duplicates.
-
-Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+Your function should return true if any value appears at least twice in the array, 
+and it should return false if every element is distinct.
 
 Example 1:
-
 Input: [1,2,3,1]
 Output: true
 */
