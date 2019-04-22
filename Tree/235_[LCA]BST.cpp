91879b15032f8a1575ab51cf58a2bf4c3ee98114
YOUNGSEOK LEE <astonisher88@gmail.com>
2019-04-22 18:04:51 -0500

    Update 235_[LCA]BST.cpp
    resursive

--- Tree/235_[LCA]BST.cpp
+++ Tree/235_[LCA]BST.cpp
@@ -1,3 +1,23 @@
 // 235. Lowest Common Ancestor of a Binary Search Tree
 // https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        int rr = root->val;
+        int pp = p->val;
+        int qq = q->val;
+        if(rr > max(pp,qq)) return lowestCommonAncestor(root->left, p, q);
+        if(rr < min(pp,qq)) return lowestCommonAncestor(root->right, p, q);
+        return root;
+    }
+};
