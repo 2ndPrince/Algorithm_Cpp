9fc2a8e3157fe75bd52ba8f0e830dd2ec4b2e2cc
YOUNGSEOK LEE <astonisher88@gmail.com>
2019-04-22 20:45:23 -0500

    mysolution added

--- WC133/1030_[Sort]Custom.cpp
+++ WC133/1030_[Sort]Custom.cpp
@@ -26,3 +26,38 @@ public:
         
     }
 };
+
+
+// My solution
+int ro = 0; // These have to be placed outside the class. Otherwise, error. I am not sure why.
+int co = 0;
+
+class Solution {
+public:
+    vector<vector<int>> allCellsDistOrder(int R, int C, int r0, int c0) {
+        ro = r0;
+        co = c0;
+        vector<vector<int>> map;
+        for(int i=0;i<R;i++){
+            for(int j=0;j<C;j++){
+                map.push_back({i,j});
+            }
+        }
+        sort(map.begin(), map.end(), customCompare);        
+        return map;
+    }
+    
+    static bool customCompare(vector<int>& p1, vector<int>& p2){
+        int d1 = abs(p1[0]-ro) + abs(p1[1]-co);
+        int d2 = abs(p2[0]-ro) + abs(p2[1]-co);
+        return d1 < d2;
+    }
+};
+
+// 1. create a map of coordinates
+// 2. define a custom sort function for distance
+// 3. sort the map with the custom sort function
+// 4. return the map data
+
+// without static: it gave error.
+// https://stackoverflow.com/questions/29286863/invalid-use-of-non-static-member-function
