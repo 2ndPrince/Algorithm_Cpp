b0e990ee2f10e2b180febab3cfbbbf6b3343fb5e
YOUNGSEOK LEE <astonisher88@gmail.com>
2019-05-02 20:23:04 -0500

    Update 518_[CoinChange2].cpp

--- DP/518_[CoinChange2].cpp
+++ DP/518_[CoinChange2].cpp
@@ -5,7 +5,7 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-        int dp[amount+1] = {0};
+        vector<int> dp(amount+1, 0); // int dp[amount+1] = {0};
         dp[0] = 1;
         for(int j=0;j<coins.size();j++){
             for(int i=1;i<=amount;i++){
